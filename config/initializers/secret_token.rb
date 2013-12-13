eadcbad3f62d007e38e6435718a85bf48831a455
Raleigh D Froeber <RDFroeber@gmail.com>
2013-12-12 21:09:32 -0500

    Bevalkyri rails app initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bevalkyri::Application.config.secret_key_base = '283e890bf5e2e63900bc38da721776e7169e0be68ab8bb4796d66217c4266892ad51205ff6d68c3e8266fecab6106e9fc77d055522b7dbe9ecdf2228001be261'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bevalkyri::Application.config.session_store :cookie_store, key: '_bevalkyri_session'
