ade6d2e97e07ac77c804ae22489e539b34f7c7e9
Raleigh D Froeber <RDFroeber@gmail.com>
2013-12-17 11:51:16 -0500

    Players table created and migrated

---
+++ db/migrate/20131217164250_create_players.rb
@@ -0,0 +1,27 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.references :user, null: false
+      t.string :name, null: false
+      t.string :kingdom
+      t.string :crest, null: false
+      t.string :color_one, null: false
+      t.string :color_two
+      t.text :manifesto
+
+      t.timestamps
+    end
+
+    # Adding a Foreign Key constraint for user_id
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL  # Executes RAW SQL
+          ALTER TABLE players
+          ADD CONSTRAINT fk_users
+          FOREIGN KEY (user_id)
+          REFERENCES users(id)
+        SQL
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216210445) do
+ActiveRecord::Schema.define(version: 20131217164250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.string   "name",       null: false
+    t.string   "kingdom"
+    t.string   "crest",      null: false
+    t.string   "color_one",  null: false
+    t.string   "color_two"
+    t.text     "manifesto"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name",             null: false
