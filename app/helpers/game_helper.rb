7773cd3a11d8e38e614c50a7a56cbe393cb5f46c
Raleigh D Froeber <RDFroeber@gmail.com>
2013-12-19 02:55:34 -0500

    GameHelper updated with tile_fill method

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -37,9 +37,40 @@ module GameHelper
     end
   end
 
-  def place_castle(board)
-    Castle.create(player_id: params[:player_id], board_id: board.id, type: "Castle", x_cord: "03", y_cord: "07")
+  def decrement(point)
+    i = point.to_i
+    i -= 1
+    num = i.to_s
+
+    if num[1] == nil
+      num = "0" + num
+    else
+      num
+    end
   end
 
+  def place_castle(board)
+    cords = ["02", "03", "04", "05", "06", "07", "08", "09", "10"]
+    x = cords.sample
+    y = cords.sample
+
+    Castle.create(player_id: params[:player_id], board_id: board.id, type: "Castle", x_cord: x, y_cord: y)
+
+    tile_fill(x,y,board)
+  end
+
+  def tile_fill(x,y,board)
+    top_right = "#{x}-#{increment(x)},#{decrement(y)}-#{y}"
+    bottom_right = "#{x}-#{increment(x)},#{y}-#{increment(y)}"
+    top_left = "#{decrement(x)}-#{x},#{decrement(y)}-#{y}"
+    bottom_left = "#{decrement(x)}-#{x},#{y}-#{increment(y)}"
+
+    tiles = [Tile.where(board_id: board.id, x_cords: top_right.split(",")[0], y_cords: top_right.split(",")[1]), Tile.where(board_id: board.id, x_cords: bottom_right.split(",")[0], y_cords: bottom_right.split(",")[1]), Tile.where(board_id: board.id, x_cords: top_left.split(",")[0], y_cords: top_left.split(",")[1]), Tile.where(board_id: board.id, x_cords: bottom_left.split(",")[0], y_cords: bottom_left.split(",")[1])]
+    # binding.pry
+    tiles.each do |tile|
+      tile[0].territory = true
+      tile[0].save
+    end
+  end
 
 end
